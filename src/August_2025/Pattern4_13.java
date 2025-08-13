8b39b0fcf827a2067ee4a6dc7835817f213ed778
ritickkumar <ritickkumar08@gmail.com>
2025-08-13 10:11:30 +0530

    pattern4_13

---
+++ src/August_2025/Pattern4_13.java
@@ -0,0 +1,12 @@
+package August_2025;
+
+public class Pattern4_13 {
+    public static void main(String[] args){
+        for(int i = 1; i <= 5 ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
